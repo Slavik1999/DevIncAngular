153d33d19281c80b2ce7c82340af29330aa3faa8
Vyachaslau <farmik222@yandex.ru>
2021-01-05 20:44:20 +0300

    Remake enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+	production: true,
+	apiKey: 'AIzaSyB_csZl5cbWmfuCFVh7rnxk4IIifiI9_ZA',
+	authDomain: 'devincubatorangular.firebaseapp.com',
+	databaseURL: 'https://devincubatorangular-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'devincubatorangular',
+	storageBucket: 'devincubatorangular.appspot.com',
+	messagingSenderId: '821700488137',
+	appId: '1:821700488137:web:f23dd7c696768d37a8d618',
+	measurementId: 'G-8FBFX1SZW1'
 };
