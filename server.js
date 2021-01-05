9bfcc44c5e9042a067c8ceb5d3fd92b04a0ca569
Vyachaslau <farmik222@yandex.ru>
2021-01-05 15:28:24 +0300

    remake server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/DevIncubatorAngular'));
+app.use(express.static(__dirname + '/dist/dev-incubator-angularr'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/dist/DevIncubatorAngular/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/dev-incubator-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
